14a5d61cbee84a14e53e16dc5cfe2fbed5bc823c
GENGIS KANHG TOLEDO RAMIREZ <TOLEDGE1@ICIDESI.MX>
2019-06-19 13:38:52 -0500

    Add hug capability

--- claseHija.cpp
+++ claseHija.cpp
@@ -3,7 +3,12 @@
 	public class Hija{
 	int edad = 40;
 	int estatura = 20;
+	int numero = 323;
 	public educar(){
 		return "yo educo";		
 	};
+	public abrazar(){
+		return "te quiero hijo";		
+	};
+
 	}
